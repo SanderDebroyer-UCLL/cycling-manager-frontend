dacbb3190145009a7300505af6be729667e377b8
BramGeboers <bramgeboers2@gmail.com>
2025-05-15 16:02:49 +0200

    Refactor navbar component to enhance user session management and add profile page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import { selectCurrentUser } from '@/features/user/user.selector';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Button } from 'primereact/button';
-import React, { useEffect, useRef } from 'react';
+import React, { use, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { User } from 'lucide-react';
 import { AppDispatch } from '@/store/store';
@@ -18,34 +18,54 @@ const navbar = () => {
 
   const dispatch = useDispatch<AppDispatch>();
 
-const logoutHandler = () => {
-  dispatch(setUser(null)); // Of een specifieke logout action als je die hebt
-  sessionStorage.removeItem('email');
-  sessionStorage.removeItem('token');
-  router.push('/'); // Redirect naar de login pagina
-};
+  useEffect(() => {
+    const email = sessionStorage.getItem('email');
+    const jwtToken = sessionStorage.getItem('jwtToken');
 
+    if (user?.email === email && user?.jwtToken == jwtToken) {
+      return;
+    }
+
+    if (email && jwtToken) {
+      dispatch(
+        setUser({
+          email: email,
+          jwtToken: jwtToken,
+        }),
+      );
+    }
+  }, [dispatch]);
+
+  const logoutHandler = () => {
+    dispatch(setUser(null)); // Of een specifieke logout action als je die hebt
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('token');
+    router.push('/'); // Redirect naar de login pagina
+  };
 
   const menuLeft = useRef<Menu>(null);
-  
+
   const items: MenuItem[] = [
     {
-        label: 'Profile',
-        items: [
-            {
-                label: 'Settings',
-                icon: 'pi pi-cog'
-            },
-            {
-                label: 'Logout',
-                icon: 'pi pi-sign-out',
-                command: () => {
-                    logoutHandler();
-                }
-            },
-        ]
-    }
-];
+      label: 'Profiel',
+      items: [
+        {
+          label: 'Instellingen',
+          icon: 'pi pi-cog',
+          command: () => {
+            router.push('/profiel');
+          },
+        },
+        {
+          label: 'Logout',
+          icon: 'pi pi-sign-out',
+          command: () => {
+            logoutHandler();
+          },
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="flex items-center justify-between text-dark-700 h-16 border-b-1 border-surface-500 backdrop-blur-md px-20">
@@ -79,10 +99,15 @@ const logoutHandler = () => {
         </div>
       )}
       {user ? (
-        <><div onClick={(event) => menuLeft.current!.toggle(event)} className='cursor-pointer' >
-          <User size={24} className="stroke-blue-500" />
-        </div><Menu model={items} popup ref={menuLeft} id="popup_menu_left" /></>
-
+        <>
+          <div
+            onClick={(event) => menuLeft.current!.toggle(event)}
+            className="cursor-pointer"
+          >
+            <User size={24} className="stroke-blue-500" />
+          </div>
+          <Menu model={items} popup ref={menuLeft} id="popup_menu_left" />
+        </>
       ) : (
         <div className="flex gap-4">
           <Link href="/authenticatie/login">
